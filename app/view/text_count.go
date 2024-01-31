b7bbd2d2b71cb4db2a13a84d981c260839698d68
David Horak <horak.david@protonmail.com>
2024-01-31 22:16:08 +0100

    added word count

--- app/view/text_count.go
+++ app/view/text_count.go
@@ -2,6 +2,7 @@ package view
 
 import (
 	"fmt"
+	"strings"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
@@ -10,21 +11,27 @@ import (
 
 func CreateCharCountContent() *fyne.Container {
 	charCountLabel := widget.NewLabel("Char count: 0")
+	wordCountLabel := widget.NewLabel("Word count: 0")
 
 	textField := widget.NewEntry()
 	textField.SetPlaceHolder("Add text to count chars")
 	textField.MultiLine = true
 	textField.Wrapping = fyne.TextWrapBreak
 	textField.OnChanged = func(s string) {
-		count := 0
+		charCount := 0
+		wordCount := 0
 		if s != "" {
-			count = len(s)
+			charCount = len(s)
+			wordCount = len(strings.Fields(s))
 		}
-		charCountLabel.SetText(fmt.Sprintf("Char count: %d", count))
+		charCountLabel.SetText(fmt.Sprintf("Char count: %d", charCount))
+		wordCountLabel.SetText(fmt.Sprintf("Word count: %d", wordCount))
 	}
 
-	return container.NewGridWithRows(2,
-		textField,
+	return container.NewBorder(nil, container.NewVBox(
 		charCountLabel,
+		wordCountLabel,
+	), nil, nil,
+		textField,
 	)
 }
